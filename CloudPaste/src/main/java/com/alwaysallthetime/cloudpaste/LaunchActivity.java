feb191a7ba7dcec99436a93cd2785882ef57a70a
Rob Brambley <rrbrambley@gmail.com>
2013-10-20 15:29:55 -0700

    use new ADNLibUtils stuff to check if user is logged in on launch
    if user is logged in, show MainActivity, else show LoginActivity

---
+++ CloudPaste/src/main/java/com/alwaysallthetime/cloudpaste/LaunchActivity.java
@@ -0,0 +1,25 @@
+package com.alwaysallthetime.cloudpaste;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.alwaysallthetime.adnlibutils.ADNSharedPreferences;
+
+public class LaunchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = null;
+        if(ADNSharedPreferences.isLoggedIn()) {
+            intent = new Intent(this, MainActivity.class);
+        } else {
+            intent = new Intent(this, LoginActivity.class);
+        }
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
+    }
+}
