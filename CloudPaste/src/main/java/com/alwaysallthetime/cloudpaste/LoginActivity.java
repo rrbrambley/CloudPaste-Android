2d3b0501c1053c361c31cf484dedb0667dbc0c48
Rob Brambley <rrbrambley@gmail.com>
2013-10-20 15:37:29 -0700

    add login layout

--- CloudPaste/src/main/java/com/alwaysallthetime/cloudpaste/LoginActivity.java
+++ CloudPaste/src/main/java/com/alwaysallthetime/cloudpaste/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.alwaysallthetime.cloudpaste;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LoginActivity extends Activity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
